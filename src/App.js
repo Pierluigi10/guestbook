46494b0555cc0010be5a097dde75866cc8b21579
pierluigi <pierluigibaiano@gmail.com>
2022-04-01 07:34:10 +0200

    create Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Guestbook from "./components/Guestbook";
 
 function App() {
   return (
     <div className="App">
-    Hello World
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/guestbook" element={<Guestbook />} />
+      </Routes>
     </div>
   );
 }
