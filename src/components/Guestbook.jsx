52da6792d9e453c20ba5de28053e69949542c497
pierluigi <pierluigibaiano@gmail.com>
2022-04-01 07:39:08 +0200

    use react-router-dom hook (useNavigate) to move between the pages

--- src/components/Guestbook.jsx
+++ src/components/Guestbook.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Guestbook() {
+  const navigate = useNavigate();
   return (
-    <div>Guestbook</div>
-  )
+    <div>
+      <h1>Guestbook</h1>
+      <button onClick={() => navigate("/")}>Home</button>
+    </div>
+  );
 }
 
-export default Guestbook
+export default Guestbook;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={() => navigate("/guestbook")}> Sign the guestbook</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
